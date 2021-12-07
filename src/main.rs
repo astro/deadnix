452759a13db3ac8ab34b74a71b8ecfc992071b99
Astro <astro@spaceboyz.net>
2021-12-07 21:40:53 +0100

    main: refactor into report

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 use std::{env::args, fs};
-use rnix::types::TypedNode;
 
 mod scope;
 mod binding;
 mod usage;
 mod dead_code;
 mod dead_code_tests;
+mod report;
 
 
 fn main() {
-    for path in args().skip(1) {
-        let content = match fs::read_to_string(&path) {
+    for file_path in args().skip(1) {
+        let content = match fs::read_to_string(&file_path) {
             Ok(content) => content,
             Err(err) => {
-                eprintln!("Error reading file: {}", err);
+                eprintln!("Error reading file {}: {}", file_path, err);
                 continue;
             }
         };
@@ -21,7 +21,7 @@ fn main() {
         let ast = rnix::parse(&content);
         let mut failed = false;
         for error in ast.errors() {
-            eprintln!("Parse error: {}", error);
+            eprintln!("Error parsing file {}: {}", file_path, error);
             failed = true;
         }
         if failed {
@@ -29,68 +29,9 @@ fn main() {
         }
 
         let results = crate::dead_code::find_dead_code(ast.node());
-        if results.len() == 0 {
-            return;
-        }
-
-        let mut lines = Vec::new();
-        let mut offset = 0;
-        while let Some(next) = content[offset..].find('\n') {
-            let line = &content[offset..offset + next];
-            lines.push((offset, line));
-            offset += next + 1;
-        }
-        lines.push((offset, &content[offset..]));
-
-        let mut last_line = 0;
-        let mut result_by_lines = Vec::new();
-        for result in results {
-            let range = result.binding.name.node().text_range();
-            let start = usize::from(range.start());
-            let line_number = lines.iter().filter(|(offset, _)| *offset <= start).count();
-            if line_number != last_line {
-                last_line = line_number;
-                result_by_lines.push((line_number, Vec::new()));
-            }
-            let result_by_lines_len = result_by_lines.len();
-            let line_results = &mut result_by_lines[result_by_lines_len - 1].1;
-            line_results.push(result);
-        }
-        for (line_number, results) in result_by_lines.iter_mut() {
-            // file location
-            println!("{}:{}:", path, line_number);
-            // line
-            println!("> {}", lines[*line_number - 1].1);
-
-            // underscores ^^^^^^^^^
-            let line_start = lines[*line_number - 1].0;
-            let mut pos = line_start;
-            print!("> ");
-            for result in results.iter() {
-                let range = result.binding.name.node().text_range();
-                let start = usize::from(range.start());
-                let end = usize::from(range.end());
-                print!("{0: <1$}{2:^<3$}", "", start - pos, "", end - start);
-                pos = end;
-            }
-            println!("");
-
-            let mut bars = String::new();
-            let mut pos = line_start;
-            for result in results.iter() {
-                let range = result.binding.name.node().text_range();
-                let start = usize::from(range.start());
-                bars = format!("{}{1: <2$}|", bars, "", start - pos);
-                pos = start + 1;
-            }
-            println!("> {}", bars);
-
-            // messages
-            for result in results.iter().rev() {
-                let range = result.binding.name.node().text_range();
-                let start = usize::from(range.start());
-                println!("> {}Unused {}", &bars[..start - line_start], result);
-            }
+        if results.len() > 0 {
+            crate::report::Report::new(file_path, &content, results)
+                .print();
         }
     }
 }
---
+++ src/report.rs
@@ -0,0 +1,92 @@
+use rnix::types::TypedNode;
+use crate::dead_code::DeadCode;
+
+pub struct LineReport {
+    line_start: usize,
+    line_number: usize,
+    line: String,
+    results: Vec<DeadCode>,
+}
+
+pub struct Report {
+    file_path: String,
+    line_reports: Vec<LineReport>,
+}
+
+impl Report {
+    /// Create a report grouped by line
+    ///
+    /// Assumes results are pre-sorted by order of appearance.
+    pub fn new(file_path: String, content: &str, results: Vec<DeadCode>) -> Self {
+        let mut lines = Vec::new();
+        let mut offset = 0;
+        while let Some(next) = content[offset..].find('\n') {
+            let line = &content[offset..offset + next];
+            lines.push((offset, line));
+            offset += next + 1;
+        }
+        lines.push((offset, &content[offset..]));
+
+        let mut last_line = 0;
+        let mut line_reports = Vec::new();
+        for result in results {
+            let range = result.binding.name.node().text_range();
+            let start = usize::from(range.start());
+            let line_number = lines.iter().filter(|(offset, _)| *offset <= start).count();
+            if line_number != last_line {
+                last_line = line_number;
+                line_reports.push(LineReport {
+                    line_start: lines[line_number - 1].0,
+                    line_number,
+                    line: lines[line_number - 1].1.to_string(),
+                    results: Vec::new(),
+                });
+            }
+            let line_results = &mut line_reports.last_mut().unwrap().results;
+            line_results.push(result);
+        }
+
+        Report {
+            file_path,
+            line_reports,
+        }
+    }
+
+    pub fn print(&self) {
+        for LineReport { line_start, line_number, line, results } in self.line_reports.iter() {
+            // file location
+            println!("{}:{}:", self.file_path, line_number);
+            // line
+            println!("> {}", line);
+
+            // underscores ^^^^^^^^^
+            let mut pos = *line_start;
+            print!("> ");
+            for result in results.iter() {
+                let range = result.binding.name.node().text_range();
+                let start = usize::from(range.start());
+                let end = usize::from(range.end());
+                print!("{0: <1$}{2:^<3$}", "", start - pos, "", end - start);
+                pos = end;
+            }
+            println!("");
+
+            let mut bars = String::new();
+            let mut pos = *line_start;
+            for result in results.iter() {
+                let range = result.binding.name.node().text_range();
+                let start = usize::from(range.start());
+                bars = format!("{}{1: <2$}|", bars, "", start - pos);
+                pos = start + 1;
+            }
+            println!("> {}", bars);
+
+            // messages
+            for result in results.iter().rev() {
+                let range = result.binding.name.node().text_range();
+                let start = usize::from(range.start());
+                println!("> {}Unused {}", &bars[..start - line_start], result);
+            }
+        }
+    }
+}
