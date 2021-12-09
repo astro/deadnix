dd04c4bf35944590cb3e13e0aad6b79983103ccc
Astro <astro@spaceboyz.net>
2021-12-09 16:44:31 +0100

    flake.nix: switch from mozillapkgs to fenix

--- flake.nix
+++ flake.nix
@@ -2,22 +2,20 @@
   inputs = {
     utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nmattia/naersk";
-    mozillapkgs.url = "github:mozilla/nixpkgs-mozilla";
-    mozillapkgs.flake = false;
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, utils, naersk, mozillapkgs }:
+  outputs = { self, nixpkgs, utils, naersk, fenix }:
     utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages."${system}";
-      mozilla = pkgs.callPackage (mozillapkgs + "/package-set.nix") {};
-      rust = (mozilla.rustChannelOf {
-        channel = "stable";
-        date = "2021-12-02";
-        sha256 = "0rqgx90k9lhfwaf63ccnm5qskzahmr4q18i18y6kdx48y26w3xz8";
-      }).rust.override {
-        #extensions = [ "clippy-preview" "rustfmt-preview" "miri-preview" ];
-        extensions = [ "clippy-preview" "rustfmt-preview" ];
-      };
+      rust = fenix.packages.${system}.stable.withComponents [
+        "cargo"
+        "rustc"
+        "rustfmt"
+        "clippy"
+      ];
 
       # Override the version used in naersk
       naersk-lib = naersk.lib."${system}".override {
