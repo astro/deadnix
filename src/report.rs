26911003e9fe18d0a5754ca306285ed37ed744a1
Astro <astro@spaceboyz.net>
2022-02-04 18:17:59 +0100

    report: convert char into byte offsets for ariadne

--- src/report.rs
+++ src/report.rs
@@ -1,7 +1,8 @@
 use ariadne::{Config, Label, Report, ReportKind, sources};
 use crate::dead_code::DeadCode;
-use rnix::types::TypedNode;
+use rnix::{TextSize, types::TypedNode};
 
+// assumes results to be sorted by occurrence in file
 pub fn print(file: String, content: &str, results: &[DeadCode]) {
     let first_result_range = results[0].binding.name.node().text_range();
     let mut builder = Report::build(
@@ -15,19 +16,39 @@ pub fn print(file: String, content: &str, results: &[DeadCode]) {
         )
         .with_message("Unused declarations were found.");
 
+    // advance into content to convert byte offsets into char offsets
+    let mut content_bytes = 0;
+    let mut content_chars = 0usize;
+    let mut char_bytes = content.chars()
+        .map(|c| usize::from(TextSize::of(c)));
     // reverse order to avoid overlapping lanes
     let mut order = results.len();
     for result in results {
+        order -= 1;
+
         let range = result.binding.name.node().text_range();
-        builder = builder.with_label(Label::new((file.clone(), range.start().into()..range.end().into()))
+        let start_byte = usize::from(range.start());
+        while content_bytes < start_byte {
+            content_bytes += char_bytes.next().unwrap();
+            content_chars += 1;
+        }
+        let start_char = content_chars;
+        let end_byte = usize::from(range.end());
+        while content_bytes < end_byte {
+            content_bytes += char_bytes.next().unwrap();
+            content_chars += 1;
+        }
+        let end_char = content_chars;
+
+        // add report label
+        builder = builder.with_label(Label::new((file.clone(), start_char..end_char))
             .with_message(format!("{}", result))
             .with_color(result.scope.color())
             .with_order(order as i32)
         );
-
-        order -= 1;
     }
 
+    // print
     builder.finish()
         .print(sources(vec![
             (file, content)
